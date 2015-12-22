5551dc85ff427df8cf1d1559e408961176a18e26
flyher <flyher@users.noreply.github.com>
2015-12-22 17:51:51 +0800

    http

---
+++ client.js
@@ -0,0 +1,67 @@
+﻿//example post
+//var http = require("http");
+//var options = {
+//    hostname: "127.0.0.1",
+//    port: 8888,
+//    path: "/a",
+//    method: "POST",
+//    headers: {
+//        "content-type":"json"
+//    }
+//}
+//var request = http.request(options, function (response) {
+
+//});
+//request.write("内容");
+//request.end();
+
+//example 
+//var http = require("http");
+//http.get("http://www.meihua.info", function (response) {
+//    var body = [];
+//    console.log(response.statusCode);
+
+//    console.log(response.headers);
+//    response.on("data", function (chunk) {
+//        body.push(chunk);
+//    });
+//    response.on("end", function () {
+//        body = Buffer.concat(body);
+//        console.log(body.toString("utf-8"));
+//    });
+//});
+
+//example url para to object
+//var querystring = require("querystring");
+//var obj= querystring.parse("foo=bar&baz=qux&baz=quux&corge");
+//console.log(obj.foo);
+
+//console.log(querystring.stringify({ foo: 'bar', baz: ['qux', 'quux'], corge: '' }));
+
+//example5 压缩处理服务器返回的数据
+var options = {
+    hostname: "127.0.0.1",
+    port: 8888,
+    path: "/",
+    method: "GET",
+    headers: {
+        "appect-encoding": "gzip,deflate"
+    }
+};
+http.request(options, function (response) {
+    var body = [];
+    response.on("data", function (chunk) {
+        body.push(chunk);
+    });
+    response.on("end", function () {
+        body = Buffer.concat(body);
+        if (response.headers["content-encoding"]==="gzip") {
+            zlib.gunzip(body, function (err,data) {
+                console.log(data.toString());
+            });
+        } else {
+            console.log(data.toString());
+        }
+    });
+
+});
---
+++ server.js
@@ -0,0 +1,47 @@
+﻿//example about http request
+//var http = require("http");
+//http.createServer(function (request, response) {
+//    response.writeHead(200, {
+//        "Content-Type":"json"
+//    });
+//    response.end("Hello World\n");
+//}).listen(8808);
+
+//example request
+//var http = require("http");
+//http.createServer(function (request, response) {
+//    var body = [];
+//    console.log(request.method);
+//    console.log(request.headers);
+//    request.on("data", function (chunk) {
+//        body.push(chunk);
+//    });
+//    request.on("end", function () {
+//        body = Buffer.concat(body);
+//        console.log(body.toString("utf-8"));
+//    });
+//}).listen(8888);
+
+//example5 压缩返回数据给客户端
+var http = require("http");
+var zlib = require("zlib");
+http.createServer(function (request,response) {
+    var i = 1024, data = "";
+    while (i--) {
+        data += ".";
+    }
+    if ((request.headers["accept-encoding"] || "").indexOf("gzip") != -1) {
+        zlib.gzip(data, function (err, data) {
+            response.writeHead(200, {
+                "content-type": "text/plain",
+                "content-encoding": "gzip"
+            });
+            response.end(data);
+        });
+    } else {
+        response.writeHead(200, {
+            "content-type":"text/plain"
+        })
+        response.end(data);
+    }
+}).listen(8888);
