3104206914486b30284b58258cc8ac69aba98430
flyher <flyher@users.noreply.github.com>
2016-02-03 18:14:32 +0800

    update

---
+++ http/hs.js
@@ -0,0 +1,47 @@
+﻿//get/post
+var http = require("http");
+var items = [];
+var server = http.createServer(function (request, response) {
+    console.log(request.url);
+    if("/"==request.url){
+        switch (request.method){
+            case "GET":
+                show(response);
+                break;
+            case "POST":
+                add(request, response);
+                break;
+            default:
+                badRequest(response);
+        }
+    } else {
+        notFound(response);
+    }
+}).listen(8888);
+
+function show(response) {
+    var html = "<html><head><title>1111</title></head>"
+        + "<body>121213213213123213123132</body></html>";
+    response.setHeader("Content-Type", "text/html");
+    response.setHeader("Content-Length",Buffer.byteLength(html));
+}
+
+function notFound(response) {
+    response.statusCode = 404;
+    response.setHeader("Content-Type", "text/html");
+    response.end("Not Found");
+}
+
+var qs = require("querystring");
+function add(request,response) {
+    var body = "";
+    request.setEncoding("utf8");
+    request.on("data", function (chunk) {
+        body += chunk;
+    });
+    request.on("end", function () {
+        var obj = qs.parse(body);
+        items.push(obj.items);
+        show(response);
+    });
+}
---
+++ http/sv.js
@@ -0,0 +1,91 @@
+﻿//var http = require("http");
+//var url = require("url");
+//var items = [];
+//var server = http.createServer(function (request, response) {
+//    response.setHeader("Access-Control-Allow-Origin", "*");
+//    switch (request.method) {
+//        case "POST":
+//            var item = "";
+//            request.setEncoding("utf8");
+//            request.on("data", function (chunk) {
+//                item += chunk;
+//            });
+//            request.on("end", function () {
+//                items.push(item);
+//                response.end("OK1\n");
+//            });
+//            break;
+//        case "GET":
+//            //items.forEach(function (item,i) {
+//            //    response.write(i+")"+item+"\n");
+//            //});
+//            //response.end("ok2");
+
+//            var body = items.map(function (item, i) {
+//                return i + ")" + item;
+//            }).join("\n");
+//            response.setHeader("Content-Length", Buffer.byteLength(body));
+//            response.setHeader("Content-Type", "text/plain;charset='utf-8'")
+//            response.end(body);
+//            break;
+//        case "DELETE":
+//            var path = url.parse(request.url).pathname;
+//            var i = parseInt(path.slice(1), 10);
+//            if (isNaN(i)) {
+//                response.statusCode = 400;
+//                response.end("Invalid item id");
+//            } else if (!items[i]) {
+//                response.statusCode = 404;
+//                response.end("Item not found");
+//            } else {
+//                items.splice(i, 1);
+//                response.end("OK\n");
+//            }
+//            break;
+//    }
+//}).listen(8888);
+
+var http = require("http");
+var parse = require("url").parse;
+var join = require("path").join;
+var fs = require("fs");
+var root = __dirname;
+var server = http.createServer(function (request,response) {
+    var url = parse(request.url);
+    //构造绝对路径
+    var path = join(root, url.pathname);
+    fs.stat(path, function (err,stat) {
+        if(err){
+            if ('ENOENT' == err.code) {
+                response.statusCode = 404;
+                response.end("Not Found");
+            } else {
+                response.statusCode = 500;
+                response.end("Internal Server Error");
+            }
+        } else {
+            response.setHeader("Content-Length", stat.size);
+            var stream = fs.createReadStream(path);
+            stream.pipe(response);
+            stream.on("error", function (err) {
+                response.statusCode = 500;
+                response.end("Internal Server Error");
+            });
+        }
+    });
+
+
+    var stream = fs.createReadStream(path);
+    //stream.on("data", function (chunk) {
+    //    response.write(chunk);
+    //});
+    //stream.on("end", function () {
+    //    response.end();
+    //});
+    stream.pipe(response);
+    stream.on("error", function (err) {
+        response.statusCode = 500;
+        response.end("Internal Server Error");
+    });
+
+}).listen(8888);
